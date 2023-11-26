9609f1d472a1d3983ecb1414903efa76079139fa
enriquedelto <enriquedelto@gmail.com>
2023-11-26 17:36:50 +0000

    Create calculo_del_area_de_un_rectangulo.py

---
+++ Básicos/Variables_y_tipos/calculo_del_area_de_un_rectangulo.py
@@ -0,0 +1,10 @@
+# Solicita al usuario que ingrese la lonitud y la anchura de un rectángulo y calcula su área (base * altura)
+
+input("Ingresa la base del rectángulo: ")
+input("Ingresa la altura del rectángulo: ")
+
+base = input
+altura = input
+area = base * altura
+
+print(f"El area del rectángulo es base ({base}) * altura ({altura}), cuyo resultado es: {area}")
