51f792d903ab0926dbdf4c84006b0ba897afe58f
enriquedelto <enriquedelto@gmail.com>
2023-11-26 17:06:43 +0000

    Create intercambio_de_valores.py

---
+++ Básicos/Variables_y_tipos/intercambio_de_valores.py
@@ -0,0 +1,14 @@
+# Crea dos variables con valores numéricos y luego escribe un programa que intercambie sus valores.
+
+x = 1
+y = 2
+
+print(f"El valor de x es = {x}")
+print(f"El valor de y es = {y}")
+
+z = x
+x = y
+y = z
+
+print(f"El valor de x es = {x}")
+print(f"El valor de y es = {y}")
