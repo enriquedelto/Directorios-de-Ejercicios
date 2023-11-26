91f265497218ab4b12f98ff3773c178df61e4080
enriquedelto <enriquedelto@gmail.com>
2023-11-26 19:17:07 +0000

    Update lista_de_compras.py
    sin terminar, mañana mas

--- Básicos/Variables_y_tipos/lista_de_compras.py
+++ Básicos/Variables_y_tipos/lista_de_compras.py
@@ -1,4 +1,17 @@
 # Crea una lista con varios ítems de compras, luego agrega, elimina y modifica elementos en la lista.
 
+def print_lista():
+    print(f"Lista de la compra actual: {', '.join(lista)}")
+
 lista = ["manzana", "platano", "sandia"]
-print(f"Lista de la compra: {', '.join(lista)}")
+print_lista()
+
+agregado = str(input("Ingresa un nuevo item: "))
+lista.append(agregado)
+
+print_lista()
+
+eliminado = str(input("Elimina un item: "))
+lista.remove(eliminado)
+
+print_lista()
