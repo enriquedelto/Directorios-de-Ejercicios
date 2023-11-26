c0139b350a977cfaf6f20ad4ee82acc681fbb27a
enriquedelto <enriquedelto@gmail.com>
2023-11-26 17:25:31 +0000

    Create concatenacion_de_cadenas.py

---
+++ Básicos/Variables_y_tipos/concatenacion_de_cadenas.py
@@ -0,0 +1,9 @@
+# Pide al usuario que infrese su nombre y apellido por separado y luego concatenalos para formar un nombre completo
+
+nombre = input("Ingrese su nombre: ")
+primer_apellido = input("Ingrese su primer apellido: ")
+segundo_apellido = input("Ingrese su segundo apellido: ")
+
+print(nombre + " " + primer_apellido + " " + segundo_apellido)
+
+print(f"Tu nombre completo es: {nombre} {primer_apellido} {segundo_apellido}")
