ae794163985f5ae93b21b2382e44b101ee95529f
enriquedelto <enriquedelto@gmail.com>
2023-11-26 18:57:16 +0000

    Update lista_de_compras.py

--- Básicos/Variables_y_tipos/lista_de_compras.py
+++ Básicos/Variables_y_tipos/lista_de_compras.py
@@ -1,4 +1,4 @@
 # Crea una lista con varios ítems de compras, luego agrega, elimina y modifica elementos en la lista.
 
 lista = ["manzana", "platano", "sandia"]
-print(f"Lista de la compra: {lista}")
+print(f"Lista de la compra: {', '.join(lista)}")
