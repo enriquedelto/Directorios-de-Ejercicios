552951277e82b444811e6962bbc7d57c5b341986
enriquedelto <enriquedelto@gmail.com>
2023-11-26 18:57:06 +0000

    Create lista_de_compras.py

---
+++ Básicos/Variables_y_tipos/lista_de_compras.py
@@ -0,0 +1,4 @@
+# Crea una lista con varios ítems de compras, luego agrega, elimina y modifica elementos en la lista.
+
+lista = ["manzana", "platano", "sandia"]
+print(f"Lista de la compra: {lista}")
