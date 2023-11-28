ce3993ca0ed2685ab32a8d84b74550b23f2d2fab
enriquedelto <enriquedelto@gmail.com>
2023-11-28 11:47:59 +0000

    mejora de robustez

--- Básicos/Variables_y_tipos/conversion_de_tipos.py
+++ Básicos/Variables_y_tipos/conversion_de_tipos.py
@@ -1,7 +1,6 @@
 # Pide al usuario que ingrese un número entero y conviértelo en un número de punto flotante. Luego, realiza la operación inversa.
 
 numero_entero = int(input("Ingresa un número entero: "))
-
 numero_entero = float(numero_entero)
 
 print(numero_entero)
