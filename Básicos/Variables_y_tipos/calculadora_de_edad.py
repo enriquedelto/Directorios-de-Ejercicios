9987ffc1aa9037bb40fbef4edba531f9d244dd52
enriquedelto <enriquedelto@gmail.com>
2023-11-28 12:19:46 +0000

    Update calculadora_de_edad.py

--- Básicos/Variables_y_tipos/calculadora_de_edad.py
+++ Básicos/Variables_y_tipos/calculadora_de_edad.py
@@ -1,16 +1,28 @@
-# Pregunta al usuario su año de nacimiento y calcula su edad actual
+# Importación del MÓDULO datetime para trabajar con fechas
+import datetime
 
-import datetime #Se importa una libreria que extrae la fecha actual del sistema
+# BLOQUE try-except para manejar errores de entrada
+try:
+    # Se solicita al usuario ingresar su año de nacimiento mediante la FUNCIÓN input y se convierte a entero mediante la FUNCIÓN int(), para luego pasar el dato a la VARIABLE año_de_nacimiento.
+    año_de_nacimiento = int(input("Ingrese su año de nacimiento: "))
+    
+    # La FUNCIÓN today() del MÓDULO datetime se utiliza para obtener la fecha actual, y se extrae el año mediante la PROPIEDAD year, asignándolo a la VARIABLE año_actual.
+    año_actual = datetime.date.today().year
 
-try: #Se abre el bloque try-except para manejar posibles errores al ingresar datos con formatos nó validos
-    año_de_nacimiento = int(input("Ingrese su año de nacimiento: ")) #Se crea una variable entera con un input que ingresa el usuario tras realizar una pregunta.
-    año_actual = datetime.date.today().year #Se extrae el dato de la fecha actual y se ingresa a la variable año_actual
-        #Comienza un "no se" que comprueba si la fecha ingresada aún siendo un formato correcto es un año de nacimiento razonable.
+    # Estructura condicional IF-ELIF-ELSE para validar el año de nacimiento.
+    # El PRIMER IF comprueba si el año de nacimiento es mayor que el año actual.
     if año_de_nacimiento > año_actual:
+        # Se muestra un mensaje de error si el año ingresado es mayor que el año actual.
         print("Ha ingresado un año de nacimiento en el futuro. Por favor, ingrese un año válido.")
+    # ELIF para comprobar si la edad calculada es mayor a 116 años.
     elif año_actual - año_de_nacimiento > 116:
+        # Se muestra un mensaje si la edad calculada es irrazonablemente alta.
         print("La edad ingresada no parece ser válida.")
+    # ELSE se ejecuta si ninguna de las condiciones anteriores es verdadera.
     else:
+        # Se calcula la edad restando el año de nacimiento del año actual y se muestra el resultado.
         print(f"Su edad actual es: {año_actual - año_de_nacimiento}")
+# EXCEPT para capturar un ValueError, que ocurre si la entrada no puede convertirse a entero.
 except ValueError:
+    # Se muestra un mensaje de error si la entrada no es un número válido.
     print("Por favor, ingrese un número válido para el año de nacimiento.")
