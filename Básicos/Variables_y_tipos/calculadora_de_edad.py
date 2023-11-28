c01c1f669ebc11182066f33f260e3d4edb18464f
enriquedelto <enriquedelto@gmail.com>
2023-11-28 11:59:59 +0000

    Update calculadora_de_edad.py

--- Básicos/Variables_y_tipos/calculadora_de_edad.py
+++ Básicos/Variables_y_tipos/calculadora_de_edad.py
@@ -1,11 +1,11 @@
 # Pregunta al usuario su año de nacimiento y calcula su edad actual
 
-import datetime
+import datetime #Se importa una libreria que extrae la fecha actual del sistema
 
-try:
-    año_de_nacimiento = int(input("Ingrese su año de nacimiento: "))
-    año_actual = datetime.date.today().year
-
+try: #Se abre el bloque try-except para manejar posibles errores al ingresar datos con formatos nó validos
+    año_de_nacimiento = int(input("Ingrese su año de nacimiento: ")) #Se crea una variable entera con un input que ingresa el usuario tras realizar una pregunta.
+    año_actual = datetime.date.today().year #Se extrae el dato de la fecha actual y se ingresa a la variable año_actual
+        #Comienza un "no se" que comprueba si la fecha ingresada aún siendo un formato correcto es un año de nacimiento razonable.
     if año_de_nacimiento > año_actual:
         print("Ha ingresado un año de nacimiento en el futuro. Por favor, ingrese un año válido.")
     elif año_actual - año_de_nacimiento > 116:
