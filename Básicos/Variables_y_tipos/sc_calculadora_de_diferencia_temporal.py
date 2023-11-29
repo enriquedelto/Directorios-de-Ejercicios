49454639b6afb6de094836e8d9a6a5ed1262b402
enriquedelto <enriquedelto@gmail.com>
2023-11-29 13:34:22 +0000

    cambiando a formato 00:00:00 WIP

--- Básicos/Variables_y_tipos/sc_calculadora_de_diferencia_temporal.py
+++ Básicos/Variables_y_tipos/sc_calculadora_de_diferencia_temporal.py
@@ -1,3 +1,16 @@
+def analizar_tiempo(tiempo_str):
+    try:
+        partes = tiempo_str.split(':')
+        if len(partes) == 3:
+            horas = int(partes[0])
+            minutos = int(partes[1])
+            segundos = int(partes[2])
+            return horas, minutos, segundos #¿?
+    except ValueError:
+        pass #¿?
+    raise ValueError("Formato de tiempo no válido. Debe ser en formato 00:00:00.") # ¿?
+
+
 try:
     tiempo_competidor_1 = int(input("Ingresa el tiempo de finalización del corredor nº 1: "))
     tiempo_competidor_2 = int(input("Ingresa el tiempo de finalización del corredor nº 2: "))
