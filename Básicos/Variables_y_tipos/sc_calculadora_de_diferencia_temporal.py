faec4a6d9769467394f2dce0fe8cf4b3f43863b2
enriquedelto <enriquedelto@gmail.com>
2023-11-29 12:22:41 +0000

    Update sc_calculadora_de_diferencia_temporal.py

--- Básicos/Variables_y_tipos/sc_calculadora_de_diferencia_temporal.py
+++ Básicos/Variables_y_tipos/sc_calculadora_de_diferencia_temporal.py
@@ -1,7 +1,12 @@
-tiempo_competidor_1 = int(input("Ingresa el tiempo de finalización del corredor nº 1: "))
-tiempo_competidor_2 = int(input("Ingresa el tiempo de finalización del corredor mº 2: "))
-diferencia = int(abs(tiempo_competidor_1 - tiempo_competidor_2))
+try:
+    tiempo_competidor_1 = int(input("Ingresa el tiempo de finalización del corredor nº 1: "))
+    tiempo_competidor_2 = int(input("Ingresa el tiempo de finalización del corredor nº 2: "))
 
-print(f"El competidor 1 finalizó la carrera en: {tiempo_competidor_1}")
-print(f"El competidor 2 finalizó la carrera en: {tiempo_competidor_2}")
-print(f"La diferencia temporal entre los resultados ha sido de {diferencia}")
+    diferencia = abs(tiempo_competidor_1 - tiempo_competidor_2)
+
+    print(f"El competidor 1 finalizó la carrera en: {tiempo_competidor_1}")
+    print(f"El competidor 2 finalizó la carrera en: {tiempo_competidor_2}")
+    print(f"La diferencia temporal entre los resultados ha sido de {diferencia}")
+
+except ValueError:
+    print("Error: Debes ingresar valores enteros para los tiempos de finalización.")
