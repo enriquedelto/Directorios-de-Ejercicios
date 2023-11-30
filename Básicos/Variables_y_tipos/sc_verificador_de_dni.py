7351a3e098627c9aa27fb8bcd54bcdd80bf88b34
enriquedelto <enriquedelto@gmail.com>
2023-11-30 13:37:04 +0000

    Update sc_verificador_de_dni.py

--- Básicos/Variables_y_tipos/sc_verificador_de_dni.py
+++ Básicos/Variables_y_tipos/sc_verificador_de_dni.py
@@ -1,4 +1,11 @@
 dni = str(input("Por favor ingrese su DNI: "))
 longitud = len(dni)
 
+if longitud > 8:
+    print("El DNI ingresado parece tener más caracteres de los válidos")
+elif longitud < 8:
+    print("El DNI ingresado parece tener menos caracteres de los válidos")
+else:
+    print("La longitud del DNI es correcta")
+
 print(longitud)
