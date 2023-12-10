82e520965cd83acd3e0582453796b5b5faf89ffe
enriquedelto <enriquedelto@gmail.com>
2023-12-10 10:51:40 +0000

    nuevo ejercicio

---
+++ Básicos/Variables_y_tipos/analizador_de_texto.py
@@ -0,0 +1,20 @@
+def contar_caracter():
+    caracter = input("Ingrese el caracter que desea contar: ")
+    contador = 0
+    for x in texto:
+        if x == caracter:
+            contador += 1
+    return contador
+
+texto = input("Por favor ingresa el texto que desea analizar: ")
+print("El caracter aparece", contar_caracter(texto), "veces en el texto.")
+
+
+'''
+print("1. Contar caracteres específicos")
+print("2. Invertir el texto")
+print("3. Extraer substring")
+print("4. Reemplazar caracteres")
+print("5. Cifrado César Básico")
+print("6 Análisis de frecuencia de caracteres")
+'''
