ce600fe1bddab52e2ef972a101d5f6c3d0a3e2dc
enriquedelto <enriquedelto@gmail.com>
2023-12-11 17:48:55 +0000

    Update analizador_de_texto.py

--- Básicos/Variables_y_tipos/analizador_de_texto.py
+++ Básicos/Variables_y_tipos/analizador_de_texto.py
@@ -15,7 +15,7 @@ def extraer_texto(texto):
     print("2. Especificando el rango de índices para extraer (inicio y fin)")
     opcion = input("Opción: ")
     if opcion == "1":
-        palabra_extraer = input("¿Qué palabra desea extraer?")
+        palabra_extraer = input("Ingrese la palabra que desea extraer: ")
         indice = texto.find(palabra_extraer)
         if indice != -1:
             return texto[indice:indice+len(palabra_extraer)]
@@ -27,12 +27,22 @@ def extraer_texto(texto):
         return texto[inicio:fin]
     else:
         return "Opción no válida"
-    
 
-texto = input("Por favor ingresa el texto que desea analizar: ")
+def reemplazar_caracteres(texto):
+    caracter_a_reemplazar = input("Ingrese el caracter que desea reemplazar: ")
+    nuevo_caracter = input("Ingrese el nuevo caracter: ")
+    texto_reemplazado = texto.replace(caracter_a_reemplazar, nuevo_caracter)
+    return texto_reemplazado
+            
+def cifrado_cesar(texto):
+
+
+
+texto = input("Ingrese el texto que desea analizar: ")
 print("El caracter aparece", contar_caracter(texto), "veces en el texto.")
 print("Texto invertido:", invertir_texto(texto))
 print("Texto extraído: ", extraer_texto(texto))
+print("Texto con caracteres reemplazados:", reemplazar_caracteres(texto))
 
 '''
 print("1. Contar caracteres específicos")
