2478ed1bb2a666b4ab0d342ffb09b58714c66892
enriquedelto <enriquedelto@gmail.com>
2023-12-17 16:31:21 +0000

    Create operadores_logicos.py

---
+++ Básicos/6. Booleanos/operadores_logicos.py
@@ -0,0 +1,3 @@
+# Escribe un programa que pida al usuario ingresar dos booleanos y luego muestre el resultado de las operaciones lógicas AND, OR y NOT aplicadas a estos valores.
+
+booleano_1 = bool(input("Ingresa \"True\" o \"False\""))
