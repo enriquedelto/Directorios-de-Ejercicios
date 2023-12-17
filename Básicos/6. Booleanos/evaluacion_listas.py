b61c8c0297306281a5c9be6470395130485aa9b9
enriquedelto <enriquedelto@gmail.com>
2023-12-17 16:19:15 +0000

    Update evaluacion_listas.py

--- Básicos/6. Booleanos/evaluacion_listas.py
+++ Básicos/6. Booleanos/evaluacion_listas.py
@@ -1,2 +1,13 @@
 # Escribe un programa que cree diferentes listas (algunas vacías y otras no) y muestre el valor booleano de cada una.
 
+def comprobador(dato):
+    if bool(dato) == False:
+        print("No hay datos en la lista")
+    else:
+        print("La lista contiene datos")
+
+lista = []
+comprobador(lista)
+
+lista.append(int(input("Ingresa un número: ")))
+comprobador(lista)
