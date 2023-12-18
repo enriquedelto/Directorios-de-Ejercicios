c2b44a0c9e8e22a1a3a78d54cac0d17be4596b58
enriquedelto <enriquedelto@gmail.com>
2023-12-18 20:19:48 +0000

    added chuletita :P

--- Básicos/6. Booleanos/operadores_logicos.py
+++ Básicos/6. Booleanos/operadores_logicos.py
@@ -1,3 +1,7 @@
 # Escribe un programa que pida al usuario ingresar dos booleanos y luego muestre el resultado de las operaciones lógicas AND, OR y NOT aplicadas a estos valores.
 
+# AND: Devuelve "True" si ambos operandos son "True".
+# OR: Devuelve "True" si al menos uno de los operandos es "True".
+# NOT: Invierte el valor del operando.
+
 booleano_1 = bool(input("Ingresa \"True\" o \"False\""))
