02202a0b3997f712fe56bc99a212bb2915366a1d
enriquedelto <enriquedelto@gmail.com>
2023-12-18 20:25:55 +0000

    Update operadores_logicos.py

--- Básicos/6. Booleanos/operadores_logicos.py
+++ Básicos/6. Booleanos/operadores_logicos.py
@@ -4,4 +4,16 @@
 # OR: Devuelve "True" si al menos uno de los operandos es "True".
 # NOT: Invierte el valor del operando.
 
-booleano_1 = bool(input("Ingresa \"True\" o \"False\""))
+def solicitar_booleano(mensaje):
+    valor = input(mensaje).strip().lower()
+    if valor in ["true", "false"]:
+        return valor == "true"
+    print("Porfavor, ingresa 'True' o 'False'.")
+
+booleano1 = solicitar_booleano("Ingresa el primer valor booleano (True/False): ")
+booleano2 = solicitar_booleano("Ingresa el segundo valor booleano (True/False): ")
+
+print(f"{booleano1} AND {booleano2} = {booleano1 and booleano2}")
+print(f"{booleano1} OR {booleano2} = {booleano1 or booleano2}")
+print(f"NOT {booleano1} = {not booleano1}")
+print(f"NOT {booleano2} = {not booleano2}")
